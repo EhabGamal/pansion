51060f601bf6ea4a8c18d144fbef2951dfc70fee
Ehab Gamal <ehab.g.sh@gmail.com>
2018-08-10 22:29:35 +0200

    Add DateInput component

---
+++ src/components/DateInput.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+
+const DateInput = props => (
+  <FormControl error={props.error} aria-describedby={`${props.id}Error`}>
+    <InputLabel htmlFor={props.id}>{props.title}</InputLabel>
+    <Input id={props.id} type="date" value={props.value} onChange={props.onChange} />
+    {props.error && <FormHelperText id={`${props.id}Error`}>Invalid date</FormHelperText>}
+  </FormControl>
+);
+
+export default DateInput;
