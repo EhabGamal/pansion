4466eb5ab4f7b7175feb340664d5f8ab7330689d
Ehab Gamal <ehab.g.sh@gmail.com>
2018-08-10 19:02:27 +0200

    Add HotelCard component

---
+++ src/components/HotelCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import PlaceIcon from "@material-ui/icons/Place";
+import { currencyFormatter } from "../utils/formatter";
+
+const HotelCard = props => (
+  <Card className="card">
+    <CardMedia
+      className="media"
+      image={`https://loremflickr.com/320/240/${props.hotel.city},city`}
+      title="Contemplative Reptile"
+    />
+    <CardContent>
+      <Typography variant="headline" color="primary">
+        {props.hotel.name}
+      </Typography>
+      <Typography variant="subheading" color="secondary" gutterBottom>
+        <PlaceIcon fontSize="inherit" />
+        {props.hotel.city}
+      </Typography>
+      <Typography variant="title">{currencyFormatter(props.hotel.price * props.nights)} AED</Typography>
+    </CardContent>
+  </Card>
+);
+
+export default HotelCard;
