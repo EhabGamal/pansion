9657c65995ea5cadab01b6ce75c2954cf3d23834
Ehab Gamal <ehab.g.sh@gmail.com>
2018-08-11 15:59:44 +0200

    Add filter and sorting to HotelsGrid component

--- src/containers/HotelsGrid.jsx
+++ src/containers/HotelsGrid.jsx
@@ -4,12 +4,28 @@ import HotelCard from "../components/HotelCard";
 
 const HotelsGrid = props => (
   <Grid container spacing={16}>
-    {props.hotels.map(hotel => (
-      <Grid item xs={12} sm={6} md={4} key={hotel.name}>
-        <HotelCard hotel={hotel} nights={props.nights} />
-      </Grid>
-    ))}
+    {props.hotels
+      .filter(filterHotels(props))
+      .sort(sortHotels(props))
+      .map(hotel => (
+        <Grid item xs={12} sm={6} md={4} key={hotel.name}>
+          <HotelCard hotel={hotel} nights={props.nights} />
+        </Grid>
+      ))}
   </Grid>
 );
+
+const sortHotels = ({ sort }) => {
+  if (!sort) return undefined;
+  let orderA = sort.order;
+  let orderB = sort.order * -1;
+  return (a, b) => a[sort.sortField] > b[sort.sortField] ? orderA : orderB;
+}
+
+const filterHotels = ({ query, range }) => {
+  return hotel => (
+    (!query || hotel.name.concat(hotel.city).toLowerCase().includes(query.toLowerCase()))
+    && hotel.price >= range.value);
+};
 
 export default HotelsGrid;
